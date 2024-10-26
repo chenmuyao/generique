723d4fc3c1166a27509e9b7df158a42fa0e197ee
Muyao CHEN <muyao.chen@intersec.com>
2024-10-26 22:01:42 +0200

    slice: add basic operations for slice

---
+++ errors.go
@@ -0,0 +1,43 @@
+// MIT License
+//
+// Copyright (c) 2024 chenmuyao
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package generique
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrEmptySlice   = errors.New("empty slice")
+	ErrNoValueFound = errors.New("no value found")
+)
+
+type ErrInvalidIndex[T any] struct {
+	Index int
+	Size  int
+}
+
+// Error implements error.
+func (e ErrInvalidIndex[T]) Error() string {
+	return fmt.Sprintf("invalid index `%d` for the slice with size of `%d`", e.Index, e.Size)
+}
